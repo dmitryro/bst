f8dc79f2dff5e74c11f0d94401dd6caa1a1b6607
Dmitry Roitman <dmitryro@gmail.com>
2022-08-21 14:32:36 -0400

    more comments

--- queue/queue.go
+++ queue/queue.go
@@ -1,13 +1,18 @@
 package queue
 
+// Generic Queue data type to be used for BFS and other searches or store
 type Queue[T any] []T
 
 
+// Enqueue a new item to the queue
 func (q *Queue[T]) Enqueue(v T) {
     /* Enqueue a new item to the queue */
 	*q = append(*q, v)
 }
 
+// Dequeue an item from the top of the queue
+// Return false if size is 0, true if successfully dequeued
+// Return the dequeued item
 func (q *Queue[T]) Dequeue() (T, bool) {
     /* Dequeue an item from the queue */
     size := len(*q)
@@ -20,6 +25,7 @@ func (q *Queue[T]) Dequeue() (T, bool) {
 	return v, true
 }
 
+// Return the current size of the queue
 func (q *Queue[T]) Size() int {
     return len(*q)
 }
